c950d1e1b8a5d816f1658e6351b865f32a3c65ba
Jose Leitao <jose.p.leitao@gmail.com>
2024-03-03 07:45:07 -0300

    02 - include get a product by id

--- app.py
+++ app.py
@@ -12,12 +12,19 @@ products = [
 app = Flask(__name__)
 
 # curl -v http://localhost:5000/products
-
 @app.route("/products")
 def get_products():
     """This function create an endpoint to show all products on json format"""
     return jsonify(products)
 
+# curl -v http://localhost:5000/product/1
+@app.route("/product/<int:id>")
+def get_product(id):
+    """This function create an endpoint to show a product based on id"""
+    product_list = [product for product in products if product['id'] == id]
+    if len(product_list) == 0:
+        return f"Product with id {id} not found", 404
+    return jsonify(product_list[0])
 
 #main()
 if __name__ == '__main__':
